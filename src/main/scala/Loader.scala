4e9869c6237fd51152d5663def7512dc01fba3c6
Andy <andrew.chung@digital.hmrc.gov.uk>
2017-09-12 11:30:59 +0100

    IO sources are now closed

--- src/main/scala/Loader.scala
+++ src/main/scala/Loader.scala
@@ -6,18 +6,29 @@ import Gen._
 trait Loader extends Any {
   def loadWeightedFile(file: String): Gen[String] = {
     val resource = this.getClass.getResource(file)
-    val data = scala.io.Source.fromURL(resource).getLines
-    val nocomments = data.filterNot(_.startsWith("#"))
-    val freqTuples = nocomments.map(_.split("\t").toList).collect {
-      case (f::w::_) => (w.filter(_.isDigit).toInt, const(f))
-    }.toSeq
-    frequency(freqTuples :_*)
+    val source = scala.io.Source.fromURL(resource)
+
+    try {
+      val data = source.getLines
+      val nocomments = data.filterNot(_.startsWith("#"))
+      val freqTuples = nocomments.map(_.split("\t").toList).collect {
+        case (f :: w :: _) => (w.filter(_.isDigit).toInt, const(f))
+      }.toSeq
+      frequency(freqTuples: _*)
+    } finally {
+      source.close
+    }
   }
 
   def loadFile(file: String): Gen[String] = {
     val resource = this.getClass.getResource(file)
-    val data = scala.io.Source.fromURL(resource).getLines
-    oneOf(data.filterNot(_.startsWith("#")).toList)
+    val source = scala.io.Source.fromURL(resource)
+    try {
+      val data = scala.io.Source.fromURL(resource).getLines
+      oneOf(data.filterNot(_.startsWith("#")).toList)
+    } finally {
+      source.close()
+    }
   }
 
 }
